dbae19c7e358ff97c511921cb49ee69242da0803
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-24 14:20:24 +0200

    Initial commit: Create new laravel project

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Validation\Rule;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderByDesc('id')->get();
+        //dd($posts);
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        //dd($categories);
+        return view('admin.posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        //dd($request->all());
+
+        // Validate data
+        $val_data = $request->validated();
+
+        // Gererate slugs
+        $slug = Post::generateSlug($request->title);
+        $val_data['slug'] = $slug;
+
+        // create resource
+        Post::create($val_data);
+        // redirect to a get route
+        return redirect()->route('admin.posts.index')->with('message', 'Post Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        //dd($categories);
+
+        return view('admin.posts.edit', compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        //dd($request->all());
+
+        // Validate data
+        $val_data = $request->validated([
+
+            'title' => ['required', Rule::unique('posts')->ignore($post)],
+            'category_id' => 'nullable|exists:categories,id',
+            'cover_image' => 'nullable',
+            'content' => 'nullable'
+
+        ]);
+
+        // Gererate slugs
+        $slug = Post::generateSlug($request->title);
+        $val_data['slug'] = $slug;
+
+        // create resource
+        Post::create($val_data);
+        // redirect to a get route
+        return redirect()->route('admin.posts.index')->with('message', 'Post Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('admin.posts.index')->with('message', "$post->title deleted successfully");
+    }
+}
---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['FrontEnd', 'BackEnd', 'Programming', 'Design', 'FullStack'];
+
+        foreach ($categories as $category) {
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($new_category->name);
+            $new_category->save();
+        }
+    }
+}
