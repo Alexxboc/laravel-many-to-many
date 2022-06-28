26ab14aa7f8eb8308d17ddbf8402504e70180760
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-28 13:10:43 +0200

    add comments

--- routes/web.php
+++ routes/web.php
@@ -254,6 +254,166 @@ $user_one->posts
 test
 
 
+    FILE STORAGE
+
+- entrare in config>filesystem 
+- modificare in public
+- tutte le immagine verranno caricate in storage>app>public 
+- Fare link simbolico tra public e storage : php artisan storage:link
+- Entrare nel file create dei post: cambiare l'input type in file ed eliminare l'old
+- nel form aggiungere attributo enctype="multipart/form-data"
+- nel metodo store verificare se la richiesta contiene il file, validare il file, salvarlo nel filesystem, recuperare la path,passare la path all'array di dati validati per il salvataggio:
+    
+    //ddd($request->hasFile('cover_image')); OPZIONE 1
+    
+    OPZIONE 2
+    if(array_key_exists('cover_image', $request->all())){
+        //valido il file
+        $request->validate([
+            'cover_image' => 'nullable|image|max:50'
+        ]);
+        //Lo salvo nel file system
+        ddd($request->all());
+        $path = Storage::put('post_images', $request->cover_image);
+        //recupero il percorso
+        //ddd($path);
+        //passare la path all'array di dati validati per il salvataggio:
+    
+        $val_data['cover_image'] = $path;
+    }
+
+    //ddd($val_data);
+
+    - Entrare nella view index per trasformare link allimmagine:
+        {{asset('storage/' . $post->cover_image)}}
+
+    - Entrare nella view show e ripetere operazione
+
+    IMPLEMENTARE METODO EDIT
+
+    - Entrare nella view edit
+
+    - Modificare input in file ed elimnare value
+
+    - nell'img {{asset('storage/' . $post->cover_image)}}
+
+    - nel form aggiungere attributo enctype="multipart/form-data"
+
+    VALIDAZIONE NEL POSTCONTROLLER
+    
+    if(array_key_exists('cover_image', $request->all())){
+        //valido il file
+        $request->validate([
+            'cover_image' => 'nullable|image|max:50'
+        ]);
+        //Lo cancello dal file system
+        Storage::delete($post->cover_image);
+
+        //ddd($request->all());
+        $path = Storage::put('post_images', $request->cover_image);
+        //recupero il percorso
+        //ddd($path);
+
+        //passare la path all'array di dati validati per il salvataggio:
+        $val_data['cover_image'] = $path;
+    }
+
+    //ddd($val_data);
+
+
+    IMPLEMENTARE METODO DELETE
+
+     Storage::delete($post->cover_image);
+     $post->delete();
+
+     return ..........
+
+
+     INVIO DELLE MAIL
+
+     - Andare nel file .env e incollare i dati da mailtrap.io
+
+     - Stoppare server e riavviarlo
+
+     - Creare oggetto mailable: php artisan make:mail NewPostCreated
+
+     - Cartella App>Mail passare:
+
+     $public $post;
+
+     nel costruttore
+     $this->post = $post
+
+     nella build
+
+     return $this
+     ->from('example.com')
+     ->subject('A new post created')
+     ->view('mail.posts-created')
+
+     - Nelle views creare cartalla mail
+     - Cartella posts
+     - File created.blade.php
+     - Mettere html con h1
+        p con strong title: {{$post->title}}          
+     
+     - Entrare nel post controller
+
+     - Nel metodo store
+
+     return new NewPostcreated($new_post)->render(); ANTEPRIMA EMAIL
+
+     - Entrare nel post controller e inviare mail
+
+     Mail::to($request->user())->send(NewPostcreated($new_post));
+
+     OPPURE
+
+     Mail::to('test@example.com')->send(NewPostcreated($new_post));
+
+     FARE NUOVA MAILABLE APPROVAZIONE AMMINISTRATORE
+
+     - php artisan make mailable PostUpdatedAdminMessage --markdown=mail.markdown.admin-postupdated
+
+     - Entrare in views>mail>markdown e inserire corpo della mail
+
+     - Entrare nel PostUpdated
+     protected $post;
+
+     nel costruttore 
+     $this->post = $post;
+
+     nel build 
+     return $this 
+     ->markdown('mail.markdown.admin-postupdated')
+     ->with([
+        'postSlug'  => $this->post->slug,
+        'postUrl' => env('APP_URL') . '/posts/' . $this->post->slug
+     ])
+
+     - Nel metodo updated
+
+     //return (new PostUpdatedAdminMessage($post))->render();
+
+     Mail::to('admin@boolpress.it')->send(new PostUpdatedAdminMessage($post));
+
+     - Creare rotta
+     Route::get('mailable', function(){
+        $post = Post::findOrFail(1);
+
+        return new PostUpdatedAdminMessage($post);
+     })
+
+
+     ADMIN RULES
+
+     nel metodo index 
+     $posts = Auth::user()->posts;
+
+
+
+
+
 
 
 */
